df148f952eb3abbe4c89d5c319d8138bd9cb66ed
Zhouxin Ge <zhouxinge@live.nl>
2020-02-25 10:01:18 +0100

    removed unnecessary indents

--- examples/nntool/mnist/mnist.c
+++ examples/nntool/mnist/mnist.c
@@ -64,16 +64,13 @@ L2_MEM uint8_t rec_digit = 0;
 // #define CAMERA
 
 //camera init parameters
-#define CAM_WIDTH    50//324
-#define CAM_HEIGHT   50//244
+#define CAM_WIDTH    100//324
+#define CAM_HEIGHT   100//244
 #define AT_CAMERA_INPUT_SIZE_BYTES (CAM_WIDTH*CAM_HEIGHT*sizeof(image_in_t))
 
 L2_MEM struct pi_device himax;
 L2_MEM struct pi_himax_conf cam_conf;
 // L2_MEM struct pi_camera_format_e cam_con;
-L2_MEM uint8_t errors = 0;
-
-// static void RunMnist()
 
 char *ImageName = NULL;
 
@@ -86,6 +83,7 @@ static void cluster()
   gap_cl_resethwtimer();
 #endif
   mnistCNN(ImageIn, ResOut);
+  // pi_uart_write(&uart, &rec_digit, 1);
   printf("Runner completed\n");
 
 #ifndef NO_IMAGE
@@ -126,170 +124,169 @@ int test_mnist(void)
 #endif
 
 // HIMAX CAMERA init, configure, execute 
-    #if defined(CAMERA)
-    ImageIn = (image_in_t *) AT_L2_ALLOC(0, AT_CAMERA_INPUT_SIZE_BYTES);
-    if (ImageIn == NULL) 
-    {
-      printf("[CAMERA] Failed to allocate memory for image\n");
-    }
+#if defined(CAMERA)
+  ImageIn = (image_in_t *) AT_L2_ALLOC(0, AT_CAMERA_INPUT_SIZE_BYTES);
+  if (ImageIn == NULL) 
+  {
+    printf("[CAMERA] Failed to allocate memory for image\n");
+  }
 
-    pi_himax_conf_init(&cam_conf);
-    cam_conf.i2c_itf = 0;
-    pi_open_from_conf(&himax, &cam_conf);
-    // cam_con.PI_CAMERA_QQVGA = 0;
+  pi_himax_conf_init(&cam_conf);
+  cam_conf.i2c_itf = 0;
+  pi_open_from_conf(&himax, &cam_conf);
+  // cam_con.PI_CAMERA_QQVGA = 0;
 
-    if (pi_camera_open(&himax))
-    {
-      printf("[CAMERA] Failed to open camera driver\n");
-      errors++;
-      goto end;
-    }
-    pi_time_wait_us(1000000);
-    pi_camera_control(&himax, PI_CAMERA_CMD_START, 0);
-    printf("[CAMERA] Start\n");
-    
-    pi_camera_capture(&himax, ImageIn, CAM_WIDTH*CAM_HEIGHT);
-    pi_camera_control(&himax, PI_CAMERA_CMD_STOP, 0);
-    printf("[CAMERA] stopped\n");
-  #endif 
+  if (pi_camera_open(&himax))
+  {
+    printf("[CAMERA] Failed to open camera driver\n");
+    goto end;
+  }
+  pi_camera_control(&himax, PI_CAMERA_CMD_START, 0);
+  printf("[CAMERA] Start\n");
+  pi_time_wait_us(1000000);
+  pi_camera_capture(&himax, ImageIn, CAM_WIDTH*CAM_HEIGHT);
+  pi_camera_control(&himax, PI_CAMERA_CMD_STOP, 0);
+  printf("[CAMERA] stopped\n");
+#endif 
 
 // Using images from PC
 #if !defined(CAMERA)
-    ImageIn = (image_in_t *) AT_L2_ALLOC(0, AT_INPUT_SIZE_BYTES);
-    if (ImageIn == NULL)
-    {
-        printf("Failed to open tensor dump file %s.\n", TENSOR_DUMP_FILE);
-        exit(-2);
-    }
-  #if !defined(NO_IMAGE)              
-    printf("Reading image\n");
-    //Reading Image from Bridge
+  ImageIn = (image_in_t *) AT_L2_ALLOC(0, AT_INPUT_SIZE_BYTES);
+  if (ImageIn == NULL)
+  {
+      printf("Failed to open tensor dump file %s.\n", TENSOR_DUMP_FILE);
+      exit(-2);
+  }
+#if !defined(NO_IMAGE)              
+  printf("Reading image\n");
+  //Reading Image from Bridge
 
-    if (!(ImageIn = (image_in_t *) AT_L2_ALLOC(0, AT_INPUT_SIZE_BYTES))) {
-        printf("Failed to allocate %ld bytes for %s\n", AT_INPUT_SIZE_BYTES, ImageName);
-        pmsis_exit(-1);
-    }
+  if (!(ImageIn = (image_in_t *) AT_L2_ALLOC(0, AT_INPUT_SIZE_BYTES))) {
+      printf("Failed to allocate %ld bytes for %s\n", AT_INPUT_SIZE_BYTES, ImageName);
+      pmsis_exit(-1);
+  }
 
-    if (ReadImageFromFile(ImageName, AT_INPUT_WIDTH, AT_INPUT_HEIGHT, AT_INPUT_COLORS, ImageIn, AT_INPUT_SIZE_BYTES, SHIFT, SHORT))
-    {
-        printf("Failed to load image %s\n", ImageName);
-        pmsis_exit(-2);
-    }    
+  if (ReadImageFromFile(ImageName, AT_INPUT_WIDTH, AT_INPUT_HEIGHT, AT_INPUT_COLORS, ImageIn, AT_INPUT_SIZE_BYTES, SHIFT, SHORT))
+  {
+      printf("Failed to load image %s\n", ImageName);
+      pmsis_exit(-2);
+  }    
 
-    printf("Finished reading image\n");
+  printf("Finished reading image\n");
 #endif  /* NO_IMAGE */
 #endif  /* NO CAMERA */
 
 // Print image
-  #if defined(PRINT_IMAGE)
-    int W = 50, H = 50;
-    for (int i=0; i<H; i++)
-    {
-        for (int j=0; j<W; j++)
-        {
-            printf("%03d, ", ImageIn[W*i + j]);
-        }
-        printf("\n");
-    }
-  #endif  /* PRINT_IMAGE */
+#if defined(PRINT_IMAGE)
+  int W = 50, H = 50;
+  for (int i=0; i<H; i++)
+  {
+      for (int j=0; j<W; j++)
+      {
+          printf("%03d, ", ImageIn[W*i + j]);
+      }
+      printf("\n");
+  }
+#endif  /* PRINT_IMAGE */
 
 // Allocate memory for output of Mnist network
-    ResOut = (short int *) AT_L2_ALLOC(0, 10 * sizeof(short int));
-    if (ResOut == NULL)
-    {
-        printf("Failed to allocate Memory for Result (%d bytes)\n", 10*sizeof(short int));
-        pmsis_exit(-3);
-    }
+  ResOut = (short int *) AT_L2_ALLOC(0, 10 * sizeof(short int));
+  if (ResOut == NULL)
+  {
+      printf("Failed to allocate Memory for Result (%d bytes)\n", 10*sizeof(short int));
+      pmsis_exit(-3);
+  }
 
 //  UART init and configure
-    pi_uart_conf_init(&uart_conf);
-    uart_conf.enable_tx = 1;
-    uart_conf.enable_rx = 0;
-    pi_open_from_conf(&uart, &uart_conf);
-    printf("UART connection opened\n");
-    if (pi_uart_open(&uart))
-    {
-        printf("Uart open failed !\n");
-        pmsis_exit(-1);
-    }
+  pi_uart_conf_init(&uart_conf);
+  uart_conf.enable_tx = 1;
+  uart_conf.enable_rx = 0;
+  pi_open_from_conf(&uart, &uart_conf);
+  printf("UART connection opened\n");
+  if (pi_uart_open(&uart))
+  {
+      printf("Uart open failed !\n");
+      pmsis_exit(-1);
+  }
 // Cluster init and configure
-    #if !defined(__EMUL__)
-    struct pi_device cluster_dev;
-    struct pi_cluster_conf cl_conf;
-    cl_conf.id = 0;
-    pi_open_from_conf(&cluster_dev, (void *) &cl_conf);
-    if (pi_cluster_open(&cluster_dev))
-    {
-        printf("Cluster open failed !\n");
-        pmsis_exit(-4);
-    }
-    #endif  /* __EMUL__ */
+#if !defined(__EMUL__)
+  struct pi_device cluster_dev;
+  struct pi_cluster_conf cl_conf;
+  cl_conf.id = 0;
+  pi_open_from_conf(&cluster_dev, (void *) &cl_conf);
+  if (pi_cluster_open(&cluster_dev))
+  {
+      printf("Cluster open failed !\n");
+      pmsis_exit(-4);
+  }
+#endif  /* __EMUL__ */
 
-    printf("Constructor\n");
-    // IMPORTANT - MUST BE CALLED AFTER THE CLUSTER IS SWITCHED ON!!!!
-    if (mnistCNN_Construct())
-    {
-        printf("Graph constructor exited with an error\n");
-        pmsis_exit(-5);
-    }
+  printf("Constructor\n");
+  // IMPORTANT - MUST BE CALLED AFTER THE CLUSTER IS SWITCHED ON!!!!
+  if (mnistCNN_Construct())
+  {
+      printf("Graph constructor exited with an error\n");
+      pmsis_exit(-5);
+  }
 
 // Cluster task
-    printf("Call cluster\n");
-    #if !defined(__EMUL__)
-    struct pi_cluster_task task = {0};
-    task.entry = cluster;
-    task.arg = NULL;
-    task.stack_size = (unsigned int) STACK_SIZE;
-    task.slave_stack_size = (unsigned int) SLAVE_STACK_SIZE;
+  printf("Call cluster\n");
+#if !defined(__EMUL__)
+  struct pi_cluster_task task = {0};
+  task.entry = cluster;
+  task.arg = NULL;
+  task.stack_size = (unsigned int) STACK_SIZE;
+  task.slave_stack_size = (unsigned int) SLAVE_STACK_SIZE;
 
-    pi_cluster_send_task_to_cl(&cluster_dev, &task);
-    #else
-    cluster();
-    #endif
-    pi_uart_write(&uart, &rec_digit, 1);
-    mnistCNN_Destruct();
+  pi_cluster_send_task_to_cl(&cluster_dev, &task);
+#else
+  cluster();
+#endif
+  pi_uart_write(&uart, &rec_digit, 1);
+  mnistCNN_Destruct();
     
 // Performance check
 #ifdef PERF
-    {
-      unsigned int TotalCycles = 0, TotalOper = 0;
-      printf("\n");
-      for (int i=0; i<(sizeof(AT_GraphPerf)/sizeof(unsigned int)); i++) {
-        printf("%45s: Cycles: %10d, Operations: %10d, Operations/Cycle: %f\n", AT_GraphNodeNames[i], AT_GraphPerf[i], AT_GraphOperInfosNames[i], ((float) AT_GraphOperInfosNames[i])/ AT_GraphPerf[i]);
-        TotalCycles += AT_GraphPerf[i]; TotalOper += AT_GraphOperInfosNames[i];
-      }
-      printf("\n");
-      printf("%45s: Cycles: %10d, Operations: %10d, Operations/Cycle: %f\n", "Total", TotalCycles, TotalOper, ((float) TotalOper)/ TotalCycles);
-      printf("\n");
-    }
+{
+  unsigned int TotalCycles = 0, TotalOper = 0;
+  printf("\n");
+  for (int i=0; i<(sizeof(AT_GraphPerf)/sizeof(unsigned int)); i++) {
+    printf("%45s: Cycles: %10d, Operations: %10d, Operations/Cycle: %f\n", AT_GraphNodeNames[i], AT_GraphPerf[i], AT_GraphOperInfosNames[i], ((float) AT_GraphOperInfosNames[i])/ AT_GraphPerf[i]);
+    TotalCycles += AT_GraphPerf[i]; TotalOper += AT_GraphOperInfosNames[i];
+  }
+  printf("\n");
+  printf("%45s: Cycles: %10d, Operations: %10d, Operations/Cycle: %f\n", "Total", TotalCycles, TotalOper, ((float) TotalOper)/ TotalCycles);
+  printf("\n");
+}
 #endif
 
 // Closing camera, uart, cluster
-  #if defined CAMERA
-  end:
-    AT_L2_FREE(0, ImageIn, AT_CAMERA_INPUT_SIZE_BYTES);
-    pi_camera_close(&himax);
-    printf("[CAMERA] closed\n");
-  #endif
-    pi_uart_close(&uart);
+#if defined CAMERA
+end:
+  AT_L2_FREE(0, ImageIn, AT_CAMERA_INPUT_SIZE_BYTES);
+  pi_camera_close(&himax);
+  printf("[CAMERA] closed\n");
+#endif
+  pi_uart_close(&uart);
 
 
 #ifndef __EMUL__    
-  // Close the cluster
-    pi_cluster_close(&cluster_dev);
+// Close the cluster
+  pi_cluster_close(&cluster_dev);
 #endif  /* __EMUL__ */
 
 #ifndef DONT_DUMP
-    dt_close_dump_file();
+  dt_close_dump_file();
 #endif
-  #ifndef CAMERA
-    AT_L2_FREE(0, ImageIn, AT_INPUT_SIZE_BYTES);
-  #endif  
-    AT_L2_FREE(0, ResOut, 10 * sizeof(short int));
-    printf("Ended\n");
 
-    pmsis_exit(0);
-    return 0;
+#ifndef CAMERA
+  AT_L2_FREE(0, ImageIn, AT_INPUT_SIZE_BYTES);
+#endif  
+  AT_L2_FREE(0, ResOut, 10 * sizeof(short int));
+  printf("Ended\n");
+
+  pmsis_exit(0);
+  return 0;
 }
 
 #if defined(__EMUL__) && !defined(LINK_IMAGE_HEADER)
