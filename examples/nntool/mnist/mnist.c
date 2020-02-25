e9009141b1ad5b5ce86528286ee47d7f0231cdd8
Zhouxin Ge <zhouxinge@live.nl>
2020-02-25 11:22:49 +0100

    removed rt_perf_t a function from the pulpos api

--- examples/nntool/mnist/mnist.c
+++ examples/nntool/mnist/mnist.c
@@ -35,6 +35,7 @@ AT_HYPERFLASH_FS_EXT_ADDR_TYPE mnist_L3_Flash = 0;
 
 // Softmax always outputs Q15 short int even from 8 bit input
 L2_MEM short int *ResOut;
+
 #ifdef QUANT_16BIT
   typedef short int image_in_t;
 #else
@@ -50,10 +51,6 @@ L2_MEM short int *ResOut;
 #define AT_INPUT_SIZE (AT_INPUT_WIDTH*AT_INPUT_HEIGHT*AT_INPUT_COLORS)
 #define AT_INPUT_SIZE_BYTES (AT_INPUT_SIZE*sizeof(image_in_t))
 
-#ifdef PERF
-L2_MEM rt_perf_t *cluster_perf;
-#endif
-
 L2_MEM image_in_t *ImageIn;
 
 //UART init param
@@ -64,8 +61,8 @@ L2_MEM uint8_t rec_digit = 0;
 // #define CAMERA
 
 //camera init parameters
-#define CAM_WIDTH    100//324
-#define CAM_HEIGHT   100//244
+#define CAM_WIDTH    200//324
+#define CAM_HEIGHT   200//244
 #define AT_CAMERA_INPUT_SIZE_BYTES (CAM_WIDTH*CAM_HEIGHT*sizeof(image_in_t))
 
 L2_MEM struct pi_device himax;
@@ -110,9 +107,13 @@ static void cluster()
 
 int test_mnist(void)
 {
-    printf("Entering main controller\n");
+  printf("Entering main controller\n");
 #ifndef DONT_DUMP
-    if (dt_open_dump_file(TENSOR_DUMP_FILE))
+  if (dt_open_dump_file(TENSOR_DUMP_FILE))
+  {
+    printf("Failed to open tensor dump file %s.\n", TENSOR_DUMP_FILE);
+    exit(-2);
+  }
 #endif 
 
 #ifdef QUANT_8BIT
@@ -171,7 +172,6 @@ int test_mnist(void)
       printf("Failed to load image %s\n", ImageName);
       pmsis_exit(-2);
   }    
-
   printf("Finished reading image\n");
 #endif  /* NO_IMAGE */
 #endif  /* NO CAMERA */
@@ -289,30 +289,34 @@ end:
   return 0;
 }
 
+static rt_event_t *   event_cluster;
+
 #if defined(__EMUL__) && !defined(LINK_IMAGE_HEADER)
 int main(int argc, char *argv[])
 {
-    if (argc < 2)
-    {
-        printf("Usage: mnist [image_file]\n");
-        exit(-1);
-    }
-    ImageName = argv[1];
-    printf("\n\n\t *** NNTOOL Mnist Example ***\n\n");
-    test_mnist();
-    return 0;
+  if (argc < 2)
+  {
+      printf("Usage: mnist [image_file]\n");
+      exit(-1);
+  }
+  ImageName = argv[1];
+  printf("\n\n\t *** NNTOOL Mnist Example ***\n\n");
+  test_mnist();
+  return 0;
 }
 #else
 int main()
 {
-    #if defined(LINK_IMAGE_NAME)
-    #define __STRING1(__s) #__s
-    #define __STRING(__s) __STRING1(__s)
-    ImageName = __STRING(LINK_IMAGE_NAME);
-    #else
-    ImageName = "../../../samples/3362_6.pgm";
-    #endif  /* LINK_IMAGE_NAME */
-    printf("\n\n\t *** NNTOOL Mnist Example ***\n\n");
-    return pmsis_kickoff((void *) test_mnist);
+  #if defined(LINK_IMAGE_NAME)
+  #define __STRING1(__s) #__s
+  #define __STRING(__s) __STRING1(__s)
+  ImageName = __STRING(LINK_IMAGE_NAME);
+  #else
+  ImageName = "../../../samples/3362_6.pgm";
+  #endif  /* LINK_IMAGE_NAME */
+  printf("\n\n\t *** NNTOOL Mnist Example ***\n\n");
+  // event_cluster = rt_event_get_blocking(NULL);
+
+  return pmsis_kickoff((void *) test_mnist);
 }
 #endif  /* (__EMUL__) && (LINK_IMAGE_HEADER) */
